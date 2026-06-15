62cb8353986c0b3d30862f233e8eab03df152155
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-15 16:33:38 +0100

    create AppController and multiple Projects and Notes for testing

--- src/main.js
+++ src/main.js
@@ -2,9 +2,42 @@ import "./style.css";
 
 import Note from "./modules/Note.js";
 import Project from "./modules/Project.js";
+import AppController from "./modules/AppController.js";
 
-const test = new Note({
-	title: "test",
-	description: "test description",
-	dueDate: { day: 25, month: 12, year: 2069 },
+const App = new AppController();
+
+const defaultProject = new Project({ name: "default" });
+App.addNewProject(defaultProject);
+
+const defaultNote = new Note({
+	title: "Default Note",
+	description: "This note is created automatically.",
+	dueDate: {
+		day: new Date().getDate(),
+		month: new Date().getMonth(),
+		year: new Date().getFullYear(),
+	},
+	priority: "high",
+});
+defaultProject.addNote(defaultNote);
+
+const testNote = new Note({
+	title: "Test Note",
+	description: "Test Note. This Note Should Not Be Left In Production!",
+	dueDate: {
+		day: new Date().getDate(),
+		month: new Date().getMonth(),
+		year: new Date().getFullYear(),
+	},
+	priority: "high",
+});
+defaultProject.addNote(testNote);
+
+const testProject = new Project({ name: "testProject" });
+App.addNewProject(testProject);
+
+testProject.addNote({
+	title: "A New Note?",
+	description: "Note Num.2",
+	dueDate: { day: 15, month: 6, year: 200 },
 });
