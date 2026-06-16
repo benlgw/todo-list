5129e1bae10c0eb0d2c8c62ae99472e5618a940e
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-16 16:50:08 +0100

    add event listeners and moved stuff into the appropriate modules

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import Project from "./modules/Project.js";
 import AppController from "./modules/AppController.js";
 import DisplayController from "./modules/DisplayController.js";
 
-const Display = new DisplayController();
 const App = new AppController();
+const Display = new DisplayController();
 
+// Initalise Default Project and Note
 const defaultProject = new Project({ name: "default" });
 App.addNewProject(defaultProject);
+Display.createProjectButton({
+	id: defaultProject.id,
+	projectName: defaultProject.name,
+});
 
 const defaultNote = new Note({
 	title: "Default Note",
@@ -22,24 +27,26 @@ const defaultNote = new Note({
 	priority: "high",
 });
 defaultProject.addNote(defaultNote);
+//  End Default Project and Note
 
-const testNote = new Note({
-	title: "Test Note",
-	description: "Test Note. This Note Should Not Be Left In Production!",
-	dueDate: {
-		day: new Date().getDate(),
-		month: new Date().getMonth(),
-		year: new Date().getFullYear(),
-	},
-	priority: "high",
+// Event Listeners
+const newProjectButton = document.querySelector("#newProject");
+newProjectButton.addEventListener("click", () => {
+	App.addNewProject({ name: "Test" });
 });
-defaultProject.addNote(testNote);
 
-const testProject = new Project({ name: "testProject" });
-App.addNewProject(testProject);
-
-testProject.addNote({
-	title: "A New Note?",
-	description: "Note Num.2",
-	dueDate: { day: 15, month: 6, year: 200 },
+const projects = document.querySelectorAll(".project");
+projects.forEach((project) => {
+	project.addEventListener("click", (e) => {
+		if (e.ctrlKey && project.textContent !== "All Projects") {
+			Display.selectMultipleButtons(project);
+		} else {
+			Display.selectButton(project);
+			if (project.textContent === "All Projects") {
+				Display.showNotes({ notes: App.getAllNotes() });
+			} else {
+				// do different stuff
+			}
+		}
+	});
 });
