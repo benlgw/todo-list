79f55b9977f71af422fd4386e803f8cd2b8a1157
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-17 15:30:38 +0100

    changed the project eventListener for a sidebar one which will detect newly added projects

--- src/main.js
+++ src/main.js
@@ -29,50 +29,53 @@ const defaultNote = new Note({
 defaultProject.addNote(defaultNote);
 //  End Default Project and Note
 
-// Initalise Test Project and Note
-const testProject = new Project({ name: "test" });
-App.addNewProject(testProject);
-Display.createProjectButton({
-	id: testProject.id,
-	projectName: testProject.name,
-});
-
-const testNote = new Note({
-	title: "Test Note",
-	description: "This note is  a test note.",
-	dueDate: {
-		day: new Date().getDate(),
-		month: new Date().getMonth(),
-		year: new Date().getFullYear(),
-	},
-	priority: "low",
-});
-testProject.addNote(testNote);
-//  End Test Project and Note
-
 // Event Listeners
 const newProjectButton = document.querySelector("#newProject");
 newProjectButton.addEventListener("click", () => {
-	App.addNewProject({ name: "Test" });
+	Display.toggleProjectModal();
 });
 
-const projects = document.querySelectorAll(".project");
-projects.forEach((project) => {
-	project.addEventListener("click", (e) => {
-		if (e.ctrlKey && project.textContent !== "All Projects") {
-			Display.selectMultipleButtons(project);
-		} else {
-			Display.selectButton(project);
-			if (project.textContent === "All Projects") {
-				Display.showNotes({ notes: App.getAllNotes() });
-			} else {
-				const buttonId = project.id;
-				const projectObject = App.getProjectById({ id: buttonId })[0];
-				const projectNotes = projectObject.getAllNotes();
-				Display.showNotes({ notes: projectNotes });
-			}
-		}
+const projectModalForm = document.querySelector("#projectModal form");
+const createProjectButton = document.querySelector("#createNewProject");
+createProjectButton.addEventListener("click", () => {
+	if (!projectModalForm.reportValidity()) return;
+	const projectTitle = projectModalForm[0].value;
+	const newProject = new Project({ name: projectTitle });
+	App.addNewProject(newProject);
+	Display.createProjectButton({
+		id: newProject.id,
+		projectName: newProject.name,
 	});
+	Display.toggleProjectModal();
 });
 
-console.log(App.getAllNotes());
+const newNoteButton = document.querySelector("#newNote");
+newNoteButton.addEventListener("click", () => {
+	if (selectedProjects.length === 0) return;
+	Display.toggleNotetModal();
+});
+
+let selectedProjects = [];
+const sidebar = document.querySelector("#sidebar");
+sidebar.addEventListener("click", (e) => {
+	const project = e.target.closest(".project");
+	if (!project) return;
+
+	if (e.ctrlKey && project.textContent !== "All Projects") {
+		selectedProjects.push(project);
+		Display.selectMultipleButtons(project);
+	} else {
+		Display.selectButton(project);
+		if (project.textContent === "All Projects") {
+			selectedProjects = [];
+			Display.showNotes({ notes: App.getAllNotes() });
+		} else {
+			selectedProjects = [];
+			selectedProjects.push(project);
+			const buttonId = project.id;
+			const projectObject = App.getProjectById({ id: buttonId })[0];
+			const projectNotes = projectObject.getAllNotes();
+			Display.showNotes({ notes: projectNotes });
+		}
+	}
+});
